613e503c0df791d808892903c1248fcd5ad8bc23
Lucca Bertoncini <luccabazzo@gmail.com>
2026-01-27 11:28:02 -0800

    Trigger benchmark workflow

--- moonfish/move_ordering.py
+++ moonfish/move_ordering.py
@@ -125,3 +125,4 @@ def mvv_lva(board: Board, move: Move, phase: float) -> float:
         move_value += evaluate_capture(board, move, phase)
 
     return -move_value if board.turn else move_value
+
